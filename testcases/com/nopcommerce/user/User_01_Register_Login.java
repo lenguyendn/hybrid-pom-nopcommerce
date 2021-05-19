0c4830dac57617721ee438a889b8325bce475735
Le Nguyen <nguyen.hongle3103@gmail.com>
2021-05-19 11:14:51 +0700

    Init automation test hybrid framework

---
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -0,0 +1,32 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class User_01_Register_Login {
+	@BeforeClass
+	public void beforeClass() {
+
+	}
+
+	@Test
+	public void TC_01_Register_Empty_Data() {
+
+	}
+
+	@Test
+	public void TC_01_Register_Invalid_Email() {
+
+	}
+
+	@Test
+	public void TC_01_Register_Invalid_Password() {
+
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		
+	}
+}
