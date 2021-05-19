cb8036d02ecf2e1ebdd5ff0f9e5c027d94365104
Le Nguyen <nguyen.hongle3103@gmail.com>
2021-05-19 23:31:18 +0700

    remove bin folder

--- testcases/com/nopcommerce/user/User_01_Register_Login.java
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -1,12 +1,20 @@
 package com.nopcommerce.user;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class User_01_Register_Login {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
 	@BeforeClass
 	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://demo.nopcommerce.com");
 
 	}
 
@@ -24,9 +32,10 @@ public class User_01_Register_Login {
 	public void TC_01_Register_Invalid_Password() {
 
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
-		
+		driver.quit();
+
 	}
 }
